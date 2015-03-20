86c9a28204ae0bdd99a494089c30be6981b94a4c
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2015-03-20 17:08:28 -0500

    datos punto 1

--- grabar_mi_nombre.c
+++ grabar_mi_nombre.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*El asume que en el computador esta instalado sox. Para interrumpir la grabacion se debe usar el comando ^C
+El archivo de audio queda guardado en la carpeta data*/
 int main(){
 	char command[100];
-	system("sox -t alsa default minombre.wav");
+	system("rec -c 1 -b 16 ./data/minombre.wav");
 	return 0;
 
 }

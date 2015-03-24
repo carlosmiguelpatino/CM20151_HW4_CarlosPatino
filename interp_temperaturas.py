b20abc85be115262e7e3c69b10bd24ff459ac550
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2015-03-23 19:07:31 -0500

    punto 1 terminado

--- interp_temperaturas.py
+++ interp_temperaturas.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
 import os as os
 import datetime as DT
 from matplotlib.dates import date2num
-from scipy.interpolate import interp1d
-from scipy.interpolate import InterpolatedUnivariateSpline
+from scipy.interpolate import interp1d, InterpolatedUnivariateSpline, KroghInterpolator
+from scipy.stats import chisquare
 import csv
 
 #Creacion de vectores de datos por ciudades
@@ -34,145 +36,93 @@ with open(filename) as f:
                 dipi.append(str(row[2]))
 
 #Conversion a formato de fecha
-fechas = []
-for i in range(len(dbog)):
-    date = DT.datetime.strptime(dbog[i], "%Y-%m-%d")
-    fechas.append(date)    
-dbog = fechas    
-    
-fechas = []
-for i in range(len(dcali)):
-    date = DT.datetime.strptime(dcali[i], "%Y-%m-%d")
-    fechas.append(date)
-dcali = fechas
+def conversion_formato(dciudad):
+    fechas = []
+    for i in range(len(dciudad)):
+        date = DT.datetime.strptime(dciudad[i], "%Y-%m-%d")
+        fechas.append(date)    
+    return fechas    
+ 
 
-fechas = []
-for i in range(len(dbquilla)):
-    date = DT.datetime.strptime(dbquilla[i], "%Y-%m-%d")
-    fechas.append(date)
-dbquilla = fechas
-
-fechas = []
-for i in range(len(dbucmng)):
-    date = DT.datetime.strptime(dbucmng[i], "%Y-%m-%d")
-    fechas.append(date)
-dbucmng = fechas
-
-fechas = []
-for i in range(len(dipi)):
-    date = DT.datetime.strptime(dipi[i], "%Y-%m-%d")
-    fechas.append(date)
-dipi = fechas
+dbog = conversion_formato(dbog)
+dcali = conversion_formato(dcali)
+dbquilla = conversion_formato(dbquilla)
+dbucmng = conversion_formato(dbucmng)
+dipi = conversion_formato(dipi)
 
 
-#Graficos de interpolaciones Bogota
-x = date2num(dbog)
-x_test = np.linspace(x[0], x[-1], 1000)
+#Para hacer interpolacion con splines el valor de x debe ir en aumento
+def ordenar(dciudad, tciudad):
+    dciudad = date2num(dciudad)  
+    dciudad, tciudad = (list(t) for t in zip(*sorted(zip(dciudad, tciudad))))
 
-fit2 = np.polyfit(x, tbog, 2)
+    return (dciudad, tciudad)
 
-f = interp1d (x, tbog)
-
-spline = InterpolatedUnivariateSpline(x, tbog)
-
-print spline(x_test)
-
-
-plt.plot(x, fit2[0]*x**2 + fit2[1]*x + fit2[2], color = 'r')
-plt.plot(x_test,f(x_test), color = 'b')
-plt.plot(x_test, spline(x_test), color = 'g')
-plt.scatter(x, tbog, c='k')
-plt.legend(['Polinomio', 'Interp1d', 'Splines' ,'data'])
-plt.show()
-
-#Graficos de interpolaciones Cali
-x = date2num(dcali)
-x_test = np.linspace(x[0], x[-1], 1000)
-
-fit2 = np.polyfit(x, tcali, 2)
-
-f = interp1d (x, tcali)
-
-spline = InterpolatedUnivariateSpline(x, tcali)
-
-
-plt.plot(x, fit2[0]*x**2 + fit2[1]*x + fit2[2], color = 'r')
-plt.plot(x_test,f(x_test), color = 'b')
-plt.plot(x_test, spline(x_test), color = 'g')
-plt.scatter(x, tcali, c='k')
-plt.legend(['Polinomio', 'Interp1d', 'Splines' ,'data'])
-plt.show()
-
-#Graficos de interpolaciones Barranquilla
-x = date2num(dbquilla)
-x_test = np.linspace(x[0], x[-1], 1000)
-
-fit2 = np.polyfit(x, tbquilla, 2)
-
-f = interp1d (x, tbquilla)
-
-spline = InterpolatedUnivariateSpline(x, tbquilla)
-
-
-plt.plot(x, fit2[0]*x**2 + fit2[1]*x + fit2[2], color = 'r')
-plt.plot(x_test,f(x_test), color = 'b')
-plt.plot(x_test, spline(x_test), color = 'g')
-plt.scatter(x, tbquilla, c='k')
-plt.legend(['Polinomio', 'Interp1d', 'Splines' ,'data'])
-plt.show()
-
-#Graficos de interpolaciones Bucaramanga
-x = date2num(dbucmng)
-x_test = np.linspace(x[0], x[-1], 1000)
-
-fit2 = np.polyfit(x, tbucmng, 2)
-
-f = interp1d (x, tbucmng)
-
-spline = InterpolatedUnivariateSpline(x, tbucmng)
-
-
-plt.plot(x, fit2[0]*x**2 + fit2[1]*x + fit2[2], color = 'r')
-plt.plot(x_test,f(x_test), color = 'b')
-plt.plot(x_test, spline(x_test), color = 'g')
-plt.scatter(x, tbucmng, c='k')
-plt.legend(['Polinomio', 'Interp1d', 'Splines' ,'data'])
-plt.show()
-
-#Graficos de interpolaciones Ipiales
-x = date2num(dipi)
-x_test = np.linspace(x[0], x[-1], 1000)
-
-fit2 = np.polyfit(x, tipi, 2)
-
-f = interp1d (x, tipi)
-
-spline = InterpolatedUnivariateSpline(x, tipi)
-
-
-plt.plot(x, fit2[0]*x**2 + fit2[1]*x + fit2[2], color = 'r')
-plt.plot(x_test,f(x_test), color = 'b')
-plt.plot(x_test, spline(x_test), color = 'g')
-plt.scatter(x, tipi, c='k')
-plt.legend(['Polinomio', 'Interp1d', 'Splines' ,'data'])
-plt.show()
+dbog, tbog = ordenar(dbog,tbog)
+dcali, tcali = ordenar(dcali,tcali)
+dbquilla, tbquilla = ordenar(dbquilla,tbquilla)
+dbucmng, tbucmng = ordenar(dbucmng,tbucmng)
+dipi, tipi = ordenar(dipi, tipi)
 
 #Calculo del error
+def calculo_error(t_true, t_pol, t_spline, t_lin, ciudad):
+    chi_pol = chisquare(t_pol, t_true)
+    chi_spline = chisquare(t_spline, t_true)
+    chi_lin = chisquare(t_lin, t_true)
 
-# def least_squares(y_data, y_calculada):
-# 	a = 0
-#     for i in range (len(y_data)):
-#         a = a + (y_data[i] - y_calculada[i])**2
-        
-#     return a
+    if chi_pol < chi_lin and chi_pol < chi_spline:
+        print "Para " + ciudad + " resulta mejor la interpolacion polinomica"
 
-# y_calculada = []
-# for i in range(len(x)):
-#     y_calculada.append(fit2[0]*x[i]**2 + fit2[1]*x[i] + fit2[2])
+    if chi_lin < chi_pol and chi_lin < chi_spline:
+        print "Para " + ciudad + " resulta mejor la interpolacion lineal"
 
-# y_calculada = []
-# for i in range(len(x)):
-#     y_calculada.append(fit2[0]*x[i]**2 + fit2[1]*x[i] + fit2[2])
+    if chi_spline < chi_lin and chi_spline < chi_pol:
+        print "Para " + ciudad + " resulta mejor la interpolacion por splines"
 
-# print "Error cuadratico para polinomio", least_squares(tbog, y_calculada)
+
+
+#Graficos interpolaciones
+
+def crear_interpolaciones(dciudad, tciudad, ciudad):
+    x = dciudad  
+    x_test = np.linspace(x[0], x[-1], 3000)
+    
+    fit5 = np.polyfit(x, tciudad, 5) #Interpolacion polinomio de grado cinco. Se escogio este grado de polinomio porque al hacer la pruebas es el que mejor se ajusta a los crecimientos y decrecimientos de la tempratura. Sin embargo no resulta practico usar polinomios de mayor grado ya que nunca se va a poder llegar a una concordoncia completa para todos los puntos
+
+    f_1 = interp1d (x, tciudad) #Interpolacion lineal
+
+    spline = InterpolatedUnivariateSpline(x, tciudad, k=3) #Interpolacion por splines
+
+    #Mensaje de mejor interpolacion
+    calculo_error(tciudad, (fit5[0]*np.asarray(x)**5 + fit5[1]*np.asarray(x)**4 + fit5[2]*np.asarray(x)**3 + fit5[3]*np.asarray(x)**2 + fit5[4]*np.asarray(x) + fit5[5]), spline(x), f_1(x), ciudad) 
+
+    fig = plt.figure()
+
+    plt.title('Temperatura de ' + ciudad)
+
+    plt.subplot(3,1,1)
+    plt.plot(x_test, fit5[0]*x_test**5 + fit5[1]*x_test**4 + fit5[2]*x_test**3 + fit5[3]*x_test**2 + fit5[4]*x_test + fit5[5] , color = 'r') 
+    plt.scatter(x, tciudad, c='k')
+    plt.legend(['Polinomio','data'])
+
+    plt.subplot(3,1,2)
+    plt.plot(x_test,f_1(x_test), color = 'b')
+    plt.scatter(x, tciudad, c='k')
+    plt.legend(['Interp1d','data'])
+
+    plt.subplot(3,1,3)
+    plt.plot(x_test, spline(x_test), color = 'g')  
+    plt.scatter(x, tciudad, c='k')
+    plt.legend(['Splines' ,'data'])
+
+    
+    plt.savefig('./data/temp_'+ciudad+'.png')
+
+
+crear_interpolaciones(dbog,tbog,"Bogota")
+crear_interpolaciones(dcali,tcali,"Cali")
+crear_interpolaciones(dbquilla,tbquilla,"Barranquilla")
+crear_interpolaciones(dbucmng,tbucmng,"Bucaramanga")
+crear_interpolaciones(dipi,tipi, "Ipiales")
+
 
